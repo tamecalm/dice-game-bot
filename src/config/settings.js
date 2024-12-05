35dae4d1e8d1b0068c4de18213f2063b090e6607
John <plumecalm@gmail.com>
2024-12-05 22:42:12 +0100

    Fix: .env && settings.js regarding bot token error

--- src/config/settings.js
+++ src/config/settings.js
@@ -1,11 +1,11 @@
 require('dotenv').config();
 
 module.exports = {
-  botToken: process.env.BOT_TOKEN,
+  botToken: process.env.TELEGRAM_BOT_TOKEN,
   dbUri: process.env.MONGO_URI,
   adminId: process.env.ADMIN_ID,
   defaultCurrency: 'NGN',
-  minimumDeposit: 1000, // Naira equivalent
+  minimumDeposit: 1, // Naira equivalent
   vatRate: 5, // VAT percentage
   paystackSecretKey: process.env.PAYSTACK_SECRET_KEY
 };
