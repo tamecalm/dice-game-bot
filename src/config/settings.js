384d9544dfd509a5fc2892d9ae2d242e7bcb4b02
John <plumecalm@gmail.com>
2024-12-06 15:21:12 +0100

    fixed admin issues

--- src/config/settings.js
+++ src/config/settings.js
@@ -3,9 +3,9 @@ require('dotenv').config();
 module.exports = {
   botToken: process.env.TELEGRAM_BOT_TOKEN,
   dbUri: process.env.MONGO_URI,
-  adminId: process.env.ADMIN_ID,
-  defaultCurrency: 'NGN',
-  minimumDeposit: 1, // Naira equivalent
+  adminIds: process.env.ADMIN_ID,
+  defaultCurrency: 'USD',
+  minimumDeposit: 1, //
   vatRate: 5, // VAT percentage
   paystackSecretKey: process.env.PAYSTACK_SECRET_KEY
 };
