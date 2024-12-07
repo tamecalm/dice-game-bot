90c7989847215b9a4eac71ef55472a1e4ec72869
John <plumecalm@gmail.com>
2024-12-07 18:35:00 +0100

    payhs

--- src/config/app.js
+++ src/config/app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const bot = require('../bot/commands/bot');  // Importing bot instance from the existing file
 const connectDb = require('./db');
 const settings = require('./settings');
-const paystackWebhook = require('../webhook/paystack');
+const paystackWebhook = require('../webhook/paystackWebhook');
 
 // Initialize the Express app
 const app = express();
